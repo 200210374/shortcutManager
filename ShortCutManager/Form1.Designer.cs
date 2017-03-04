47affb005da945caabf1b567fa45dd5612571340
brandonroycstn <brandonroycstn@hotmail.com>
2017-03-03 20:26:49 -0600

    project created and mostly finished

--- ShortCutManager/Form1.Designer.cs
+++ ShortCutManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShortCutManager
 {
-    partial class Form1
+    partial class shortcutManagerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.shortcutListBox = new System.Windows.Forms.ListBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loadSaveButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(333, 146);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(101, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "&Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(333, 234);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(101, 23);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "&Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(333, 457);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(101, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "&Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(333, 192);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(101, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "&Add Shortcut";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // shortcutListBox
+            // 
+            this.shortcutListBox.FormattingEnabled = true;
+            this.shortcutListBox.Items.AddRange(new object[] {
+            ""});
+            this.shortcutListBox.Location = new System.Drawing.Point(3, 100);
+            this.shortcutListBox.Name = "shortcutListBox";
+            this.shortcutListBox.Size = new System.Drawing.Size(288, 446);
+            this.shortcutListBox.TabIndex = 5;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(333, 321);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(101, 23);
+            this.saveButton.TabIndex = 6;
+            this.saveButton.Text = "&Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(333, 274);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(101, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "&Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Click on a record in the box below to select that record";
+            // 
+            // loadSaveButton
+            // 
+            this.loadSaveButton.Location = new System.Drawing.Point(333, 100);
+            this.loadSaveButton.Name = "loadSaveButton";
+            this.loadSaveButton.Size = new System.Drawing.Size(101, 23);
+            this.loadSaveButton.TabIndex = 10;
+            this.loadSaveButton.Text = "&Load last  save";
+            this.loadSaveButton.UseVisualStyleBackColor = true;
+            this.loadSaveButton.Click += new System.EventHandler(this.loadSaveButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(333, 363);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(101, 23);
+            this.editButton.TabIndex = 11;
+            this.editButton.Text = "&Edit all";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(333, 410);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(101, 23);
+            this.helpButton.TabIndex = 12;
+            this.helpButton.Text = "&Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // shortcutManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(467, 563);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.loadSaveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.shortcutListBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.openButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "shortcutManagerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shortcut Manager";
+            this.Load += new System.EventHandler(this.shortcutManagerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox shortcutListBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loadSaveButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button helpButton;
     }
 }
 
--- ShortCutManager/Form1.cs
+++ ShortCutManager/Form1.cs
@@ -6,15 +6,205 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
+
+
 
 namespace ShortCutManager
 {
-    public partial class Form1 : Form
+    public partial class shortcutManagerForm : Form
     {
-        public Form1()
+        public shortcutManagerForm()
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            //need to change delete
+
+         
+
+
+            for (int v = 0; v < shortcutListBox.SelectedItems.Count; v++)
+            {
+                shortcutListBox.Items.Remove(shortcutListBox.SelectedItems[v]);
+            }
+
+
+
+            shortcutListBox.Items.Remove(shortcutListBox.SelectedIndex);
+
+
+
+        }
+
+
+
+
+
+
+        private void shortcutManagerForm_Load(object sender, EventArgs e)
+        {
+
+
+        
+
+
+          
+
+
+        }
+          
+     
+
+
+           
+           
+
+        
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            const string sPath = "Shortcut_Items.txt";
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in shortcutListBox.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+
+            MessageBox.Show("saved!");
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+
+
+ if (shortcutListBox.SelectedIndex == -1) 
+            {
+                DialogResult dialogResult = MessageBox.Show("You did not select a shortcut to open");
+
+            }
+
+         else {
+                try
+                {
+                    string file = shortcutListBox.SelectedItem.ToString();
+                    Process.Start(file);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("You did not select a shortcut to open");
+                }
+
+
+        }   
+
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure? All shortcuts will be lost.", "Clear?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                shortcutListBox.DataSource = null;
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do nothing
+            }
+            
+
+
+        }
+        
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            
+
+              
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                string path = "Shortcut_Items.txt";
+                string tempPath = "";
+                tempPath = openFileDialog1.FileName;
+
+         
+                
+                string appendText = Environment.NewLine + tempPath + Environment.NewLine;
+
+                File.AppendAllText(path, appendText);
+
+        //        MessageBox.Show("Shortcut added");
+
+
+
+
+                
+                    }
+                }
+
+        private void loadSaveButton_Click(object sender, EventArgs e)
+        {
+            
+
+
+                if (File.Exists("Shortcut_Items.txt"))
+                {
+                    shortcutListBox.DataSource = File.ReadAllLines("Shortcut_Items.txt");
+                   
+                }
+
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("Shortcut_Items.txt");
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("Readme.txt");
+        }
     }
-}
+
+
+
+
+
+
+           
+            
+
+
+
+        }
+    
